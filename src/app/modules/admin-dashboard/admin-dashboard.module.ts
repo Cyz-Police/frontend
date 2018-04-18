0de7fc3582c11d0763471253acbaab64330ae481
Dominick Seputis <dom.seputis@gmail.com>
2018-04-18 23:18:17 +0300

    Changed colorscheme

--- src/app/modules/admin-dashboard/admin-dashboard.module.ts
+++ src/app/modules/admin-dashboard/admin-dashboard.module.ts
@@ -6,9 +6,15 @@ import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { CountysComponent } from './countys/countys.component';
+import { UsersComponent } from './users/users.component';
 
 const Routes = [
-  { path: 'dashboard', component: AdminDashboardComponent }
+  { path: 'dashboard', component: AdminDashboardComponent,
+    children: [
+      { path: 'countys', component: CountysComponent },
+      { path: 'users', component: UsersComponent }
+    ]}
 ];
 
 @NgModule({
@@ -16,7 +22,7 @@ const Routes = [
     CommonModule,
     RouterModule.forRoot(Routes)
   ],
-  declarations: [HeaderComponent, NavbarComponent, AdminDashboardComponent],
+  declarations: [HeaderComponent, NavbarComponent, AdminDashboardComponent, CountysComponent, UsersComponent],
   exports: [RouterModule]
 })
 export class AdminDashboardModule { }
