621dbb26bbafa9b85b2aac78ac46bcf24759f893
Dominick Seputis <dom.seputis@gmail.com>
2018-04-27 08:16:26 +0300

    Fixes

--- src/app/modules/admin-dashboard/toast/toast.component.ts
+++ src/app/modules/admin-dashboard/toast/toast.component.ts
@@ -11,7 +11,7 @@ export class ToastComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log('Perduotas pranesimas: ',this.message);
+    console.log('Perduotas pranesimas: ', this.message);
   }
 
 }
