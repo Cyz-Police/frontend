679654a05cb8f2b47a0383c88e03e95684477c0f
Dominick Seputis <dom.seputis@gmail.com>
2018-05-14 20:30:35 +0300

    file download

--- src/app/modules/dashboard/components/list/list.component.ts
+++ src/app/modules/dashboard/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  private dateFrom: Date;
+  private dateTo: Date;
 
-  constructor() { }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit() {
+  }
+
+  getList() {
+    this.itemService.getList(this.dateFrom, this.dateTo).subscribe(
+      file => {
+        const fileName = `sarasas-${this.dateFrom}-${this.dateTo}`;
+        const objectUrl: string = URL.createObjectURL(file);
+        const a: HTMLAnchorElement = document.createElement('a') as HTMLAnchorElement;
+        a.href = objectUrl;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();        
+    
+        document.body.removeChild(a);
+        URL.revokeObjectURL(objectUrl);
+      },
+      err => alert(err)
+    );
   }
 
 }
--- src/app/modules/dashboard/services/item.service.ts
+++ src/app/modules/dashboard/services/item.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AuthenticationService } from '../../../authentication/authentication.service';
 import Constants from '../../../config/constants';
 
 import { Item } from './../interfaces/item';
@@ -9,13 +10,36 @@ import { Observable } from 'rxjs/Observable';
 export class ItemService {
   private mainUrl = `${Constants.host}/item`;
   private serachUrl = `${this.mainUrl}/getById`;
+  private pullUrl = `${this.mainUrl}/pull`;
+  
+  private options = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.authService.getToken()
+    })
+  };
+  
+  private fileOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.authService.getToken()
+    }),
+    responseType: 'text' as 'text'
+  };
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthenticationService,
+  ) { }
 
   getById(markId): Observable<Item> {
     return this.http.get(`${this.serachUrl}/${markId}`).map(res => res).catch(this.handleError);
   }
-  
+
+  getList(dateFrom: Date, dateTo: Date) {
+    return this.http.get(`${this.pullUrl}/${dateFrom}/${dateTo}`,this.fileOptions).map(res => new Blob([res],{ type: 'text/csv' })).catch(this.handleError)
+  }
+
   private handleError (error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
